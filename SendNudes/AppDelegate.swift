ea69a9eda28b3a63857d3c6c3ace5f34d64cfe01
A. Vatsaev <azero79@gmail.com>
2016-12-05 21:35:43 +0100

    Initial Commit

---
+++ SendNudes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SendNudes
+//
+//  Created by Aslan Vatsaev on 05/12/2016.
+//  Copyright © 2016 Aslan Vatsaev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SendNudes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SendNudes
+//
+//  Created by Aslan Vatsaev on 05/12/2016.
+//  Copyright © 2016 Aslan Vatsaev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
